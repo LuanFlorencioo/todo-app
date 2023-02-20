e096f507a38deae64ecc5c84b17d8c88dfc1e954
Luan F. Silva <luanflorencio87@gmail.com>
2023-02-19 21:11:04 -0300

    initial commit

---
+++ src/database/database.interface.ts
@@ -0,0 +1,5 @@
+export interface iTask {
+  title: string
+  isChecked: boolean
+  created: Date
+}
---
+++ src/database/index.ts
@@ -0,0 +1,5 @@
+import { iTask } from "./database.interface";
+
+const database: iTask[] = [];
+
+export default database;
